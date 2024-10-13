74e6c343b7d0c3b6b0f9ed1ce955d600eaed64b2
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-13 22:54:13 +0600

    add say hello

--- A_Summation.c
+++ A_Summation.c
@@ -16,3 +16,17 @@ int main()
     printf("%lld\n",llabs(sum));
     return 0;
 }
+//Question
+//Given a number N and an array A of N numbers. Print the absolute summation of these numbers.
+
+// absolute value : means to remove any negative sign in front of a number .
+
+// EX : |-5| = 5 , |7| = 7
+
+// Input
+// First line contains a number N (1 ≤ N ≤ 105) number of elements.
+
+// Second line contains N numbers (-109  ≤  Ai  ≤  109).
+
+// Output
+// Print the absolute summation of these numbers.
---
+++ HackerRank/SayHello.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    char inputString[100];  // Create a character array to hold the input string
+    
+    // Read the input string
+    scanf("%[^\n]", inputString);  // Read the whole line including spaces until a newline character is encountered
+    
+    // Print the fixed output
+   
+       // Print "Hello, World!"
+    
+   
+    
+    return 0;
+}
